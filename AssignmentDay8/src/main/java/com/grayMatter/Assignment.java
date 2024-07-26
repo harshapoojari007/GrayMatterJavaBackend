35b63386572dea7df67b5438908be7cef28c74a6
Harsha Poojari <harsha.poojari@graymatter.co.in>
2024-07-26 09:26:00 +0200

    Update Assignment 8 Menu Driven JDBC

--- AssignmentDay8/src/main/java/com/grayMatter/Assignment.java
+++ AssignmentDay8/src/main/java/com/grayMatter/Assignment.java
@@ -21,53 +21,74 @@ public static void main(String[] args) throws ClassNotFoundException, SQLExcepti
 	
 	boolean ch=true;
 	do {
-		System.out.println("1. Update\n2. Select\n3. Delete\n4. Exit");
+		System.out.println("1. Insert\n2. Update\n3. Select\n4. Delete\n5. Exit");
 		Scanner sc=new Scanner(System.in);
 		int c=sc.nextInt();
 		switch(c) {
-		case 1:String query="update regions set region_name=? where region_id=?";
-		PreparedStatement ps=con.prepareStatement(query);
-		ps.setString(1, "Karnataka");
-		ps.setInt(2, 7);
-		ps.executeUpdate();
-		
-		Statement st=con.createStatement();
-		String query1="select * from regions";
-		ResultSet rs=st.executeQuery(query1);
-		System.out.println("Updated");
-		while(rs.next())
-			System.out.println(rs.getInt(1)+" "+rs.getString(2));
+		case 1:
+			String iQuery="Insert into regions values(?,?)";
+			PreparedStatement ins=con.prepareStatement(iQuery);
+			System.out.print("Enter Region Id:");
+			int id=sc.nextInt();
+			System.out.println("Enter Region Name:");
+			String name=sc.next();
+			ins.setInt(1, id);
+			ins.setString(2, name);
+			ins.executeUpdate();
+			System.out.println("Successfully Inserted Values");
+			break;
+		case 2:
+			String query="update regions set region_name=? where region_id=?";
+		     PreparedStatement ps=con.prepareStatement(query);
+		     System.out.print("Enter Region Id To be Updated:");
+			 int uid=sc.nextInt();
+			 System.out.println("Enter Region Name To be Added:");
+			 String uname=sc.next();
+		     ps.setString(1, uname);
+		     ps.setInt(2, uid);
+		     ps.executeUpdate();
+		     System.out.println("Successfully Updated");
+		     break;
+		case 4:
+			query ="delete from regions where region_id=?";
+	        	ps=con.prepareStatement(query);
+	        	ps.setInt(1,7);
+	        	ps.executeUpdate();
+
+               System.out.println("row deleted successfully");
+               break;
+		case 3:	
+			 Statement st1=con.createStatement();
+			 String query3= "Select * from regions";
+		       ResultSet rs1= st1.executeQuery(query3);
+		     System.out.println("Region Id   Region_Name");
+			 while(rs1.next()) {
+			 System.out.println(rs1.getInt(1)+"           "+rs1.getString(2));
+			 }
+			
+            break;
+		case 5:
+			ch=false;break;
+		default:
+			System.out.println("Invalid CHoice");
 		
 		}
+	
+		
+		
+
 		
 	}while(ch);
 	
 	
-//	query ="delete from regions where region_id=?";
-//
-//			ps=con.prepareStatement(query);
-//
-//			ps.setInt(1, 7);
-//
-//			ps.executeUpdate();
-//
-//			System.out.println("row deleted");
-//
-//			String query3= "Select * from regions";
-//
-//			ResultSet rs1= st.executeQuery(query3);
-//
-//			while(rs1.next()) {
-//
-//				 System.out.println(rs1.getInt(1)+" "+rs1.getString(2));
-//
-//			}
-//	
-//	ps.close();
-//	st.close();
-//	rs.close();
-//	
-	
+////	
+////
+////			
+////
+////			ps.setInt(1, 7);
+////
+////			
+////
 
 }
 }
